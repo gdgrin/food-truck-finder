724217396dce28ca1a506691cb85b698b3937a9d
gdgrin <genedgrinberg@gmail.com>
2022-01-23 18:00:20 -0500

    regex and twilio integration

--- app.py
+++ app.py
@@ -1,26 +1,47 @@
 from flask import Flask, request, redirect
-from src.food_truck_info import get_n_closest_trucks
-# from twilio.twiml.messaging_response import MessagingResponse
+from food_truck_info import get_n_closest_trucks
+from twilio.twiml.messaging_response import MessagingResponse
+import re
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def hello():
-    results = get_n_closest_trucks(37, -140, 5)
-    return results
+    # body = request.values.get('Body', None)
 
-# @app.route("/sms", methods=['POST'])
-# def incoming_sms():
-#     """Send a dynamic reply to an incoming text message"""
+    # resp = ''
+
+    # exp = re.compile("-?\d{1,3}(\.\d+)?,-?\d{1,3}(\.\d+)?")
+    # regex_check = exp.match(body)
+    # if regex_check != None:
+    #     vals = body.split(",")
+    #     lat = float(vals[0].strip())
+    #     lon = float(vals[1].strip())
+    #     resp = get_n_closest_trucks(lat,lon,5)
+    # else:
+    #     resp = "Please enter a message in the format <latitude,longitude> (i.e. 37,-140)"
+
+    # return resp
+    return 'Test'
     
-#     body = request.values.get('Body', None)
 
-#     resp = MessagingResponse()
+@app.route("/sms", methods=['POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    
+    body = request.values.get('Body', None)
 
-#     if body == 'hello':
-#         resp.message("Hi!")
-#     elif body == 'bye':
-#         resp.message("Goodbye")
+    resp = MessagingResponse()
 
-#     return str(resp)
+    exp = re.compile("-?\d{1,3}(\.\d+)?,-?\d{1,3}(\.\d+)?")
+    regex_check = exp.match(body)
+    if regex_check != None:
+        vals = body.split(",")
+        lat = float(vals[0].strip())
+        lon = float(vals[1].strip())
+        resp.message(get_n_closest_trucks(lat,lon,5))
+    else:
+        resp.message("Please enter a message in the format <latitude,longitude> (i.e. 37,-140)")
+
+    return str(resp)
